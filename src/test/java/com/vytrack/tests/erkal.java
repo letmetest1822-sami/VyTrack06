5e7e939ab49d6265bdf436f0e9e8719da4992c31
erkal38 <nalruvceysem1984@gmail.com>
2020-05-23 17:10:59 +0300

    update erkal.java

--- src/test/java/com/vytrack/tests/erkal.java
+++ src/test/java/com/vytrack/tests/erkal.java
@@ -3,6 +3,7 @@ package com.vytrack.tests;
 public class erkal {
     public static void main(String[] args) {
         System.out.println("get.la");
+        System.out.println("caner kanka");
     }
 }
 
