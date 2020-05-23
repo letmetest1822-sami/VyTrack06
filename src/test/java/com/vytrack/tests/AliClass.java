8c81438b39d4b5fc078c5abc212fe977dda52e36
saerdem <saerdem@gmail.com>
2020-05-23 17:19:27 +0300

    1. update

--- src/test/java/com/vytrack/tests/AliClass.java
+++ src/test/java/com/vytrack/tests/AliClass.java
@@ -3,5 +3,7 @@ package com.vytrack.tests;
 public class AliClass {
     public static void main(String[] args) {
         System.out.println("Hello Group");
+        System.out.println("1. update has done");
+
     }
 }
